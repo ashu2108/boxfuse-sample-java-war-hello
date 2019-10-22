4a213c7666349fcc4fd98fc7593a72bbb30fc96a
ashu2108 <50979641+ashu2108@users.noreply.github.com>
2019-10-22 15:20:41 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,10 +3,10 @@ node
   stage('build stage')
   {
     git 'https://github.com/ashu2108/boxfuse-sample-java-war-hello.git'
-   sh ''' mvn package '''
+   sh '''sudo mvn package '''
   }
   stage('Deploy stage')
   {
-    sh ' cp target/*.war /var/www/html/ '
+    sh 'sudo cp target/*.war /var/www/html/ '
   }
 }
