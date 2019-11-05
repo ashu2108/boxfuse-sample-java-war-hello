2abc38992a9dba1bbf6746c1ecb342c26e3be81e
ashu2108 <50979641+ashu2108@users.noreply.github.com>
2019-11-05 11:43:34 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,12 +1,16 @@
-node
-{
-  stage('build stage')
-  {
-    git 'https://github.com/ashu2108/boxfuse-sample-java-war-hello.git'
-   sh '''sudo mvn package '''
-  }
-  stage('Deploy stage')
-  {
-    sh 'sudo cp target/*.war /usr/share/tomcat/webapps/'
-  }
+node {
+    stage("git clone"){
+        checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/ashu2108/boxfuse-sample-java-war-hello.git']]])
+    }
+    stage("maven build"){
+        sh "mvn clean install"
+        sh "cp /var/lib/jenkins/workspace/assigmnet1/target/*.war /var/lib/tomcat/webapps"
+    }
+    stage("Docker"){
+        sh "docker --version"
+        sh "docker build -t assignment2 ."
+        sh "docker tag assignment2:latest ashutoshteknur21/assignment2:latest"
+        sh "docker push ashutoshteknur21/assignment2:latest"
+        sh "docker-compose up -d"
+    }
 }
